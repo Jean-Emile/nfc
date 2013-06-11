e67f0d07bffd5e2d993c97a5d7c3d94b9477edf6
JeD <jedartois@gmail.com>
2013-06-11 16:19:30 +0200

    initial commit

---
+++ src/main/java/org/kevoree/android/nfc/api/INfc.java
@@ -0,0 +1,11 @@
+package org.kevoree.android.nfc.api;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jed
+ * Date: 11/06/13
+ * Time: 16:17
+ * To change this template use File | Settings | File Templates.
+ */
+public interface INfc {
+}
